18b813565b18d2843d123cb0e3e0ca2f1540c257
Michal Rodzen <michalrodzen1994@gmail.com>
2018-02-27 23:15:11 +0100

    Ad code

---
+++ 10.2/js/scripts.js
@@ -0,0 +1,18 @@
+$(function(){
+    var carouselList = $("#carousel ul");
+    
+	setInterval(changeSlide, 3000);
+	
+	
+	function moveFirstSlide () {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		
+		$(lastItem).after(firstItem);
+		$(carouselList).css('margin-left', '0');
+	}
+		function changeSlide() {
+		carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);	
+	}
+	
+});
